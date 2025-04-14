2a40db934548839a3dd2865f7ff39dba83abe3a1
prashant <prashantchouhan711@gmail.com>
2025-04-14 22:56:32 +0530

    done ab

--- components/ClientRoot.tsx
+++ components/ClientRoot.tsx
@@ -13,7 +13,7 @@ export default function ClientRoot({ children }: { children: React.ReactNode }) 
     <SessionProvider>
       <ReactQueryProvider>
         <SidebarProvider>
-          <div className="grid grid-cols-[auto_1fr] overflow-x-hidden min-h-screen bg-main/10 w-screen ">
+          <div className="grid grid-cols-[auto_1fr] overflow-x-hidden min-h-screen bg-gradient-to-r from-white to-main/50 w-screen ">
             <AppSidebar />
             <div className="flex flex-col overflow-x-hidden w-full">
               <AppHeader />
