78684eb342eb1f9a58bd510bf6e6840ad5e2fe81
prashant <prashantchouhan711@gmail.com>
2025-04-14 21:20:34 +0530

    sas

--- app/(root)/home/page.tsx
+++ app/(root)/home/page.tsx
@@ -1,7 +1,7 @@
 "use client";
-import HomePage from "@/pages/home-page"
+import HomePage from "@/pages/home-page";
+export const dynamic = "force-dynamic";
 
 export default function Home() {
-
-  return <HomePage />
+  return <HomePage />;
 }
